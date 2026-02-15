da2324351773b147a2e3275fda2fc4df0a21a604
irom999 <morizei0719@gmail.com>
2026-02-16 01:27:22 +0900

    About page delete

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -4,11 +4,6 @@
 	import * as DarkMode from 'svelte-fancy-darkmode';
 	import MoonToSunny from '~icons/line-md/moon-filled-to-sunny-filled-loop-transition';
 	import SunnyToMoon from '~icons/line-md/sunny-filled-loop-to-moon-filled-transition';
-
-	const LINKS = [
-		{ name: 'blog', href: resolve('/blog') },
-		{ name: 'about', href: resolve('/about') },
-	] as const;
 </script>
 
 {#snippet underline(isPath: boolean, transparentDefault = false)}
